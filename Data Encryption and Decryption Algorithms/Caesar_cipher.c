24bd7a0f6cb4bd2233fada9b0726f9a7ea0774f7
Nivesh S <126408941+in-explicable@users.noreply.github.com>
2024-07-17 11:09:10 +0530

    Update Caesar_cipher.c

--- Data Encryption and Decryption Algorithms/Caesar_cipher.c
+++ Data Encryption and Decryption Algorithms/Caesar_cipher.c
@@ -1,50 +1,45 @@
-#include<stdio.h>
-int main()
-{
-        char message[100], ch;
-        int i,key;
-        printf("Enter a message to encrypt:");
-        scanf("%s",message);
-        printf("Enter key:");
-        scanf("%d",&key);
-        for(i=0;message[i]!='\0';++i) {
-                ch = message[i];
-                if(ch>='a' && ch<='z') {
-                        ch=ch+key;
-                        if(ch>'z') {
-                        ch=ch+ch-'z'+'a'-1;
-                        }
-                        message[i]=ch;
-                }
-                else if(ch>='A' && ch<='Z') {
-                        ch=ch+key;
-                        if(ch>'Z') {
-                        ch=ch-'Z'+'A'-1;
-                        }
-                        message[i]=ch;
-                }
-         }        
-        printf("Encrypted Message is: %s", message);
-        
-        for(i=0;message[i]!='\0';++i) {
-                ch = message[i];
-                if(ch>='a' && ch<='z') {
-                        ch=ch-key;
-                        if(ch>'z') {
-                        ch=ch+'z'-'a'+1;
-                        }
-                        message[i]=ch;
-                }
-                else if(ch>='A' && ch<='Z') {
-                        ch=ch+key;
-                        if(ch>'Z') {
-                        ch=ch+'Z'-'A'+1;
-                        }
-                        message[i]=ch;
-                }
-         }        
-        printf("\nDecrypted message is: %s", message);
-        
-        
-        return 0;                       
+#include <stdio.h>
+int i,key;
+char msg[100], encode[100], decode[100];
+void getDetails(){
+	printf("Enter your message : ");
+	scanf("%s",msg);
+	printf("Enter the key : ");
+	scanf("%d",&key);
+	key = key%26;
+}
+char encryptLetter(char letter,int key, int limit){
+	if((letter+key) > limit)
+		return (letter+key-26);
+	return (letter+key);
+}
+char decryptLetter(char letter,int key, int limit){
+	if((letter-key) < limit)
+		return (letter - key + 26);
+	return (letter-key);
+}
+
+
+
+int main(){
+	getDetails();
+	for(i=0;msg[i] != '\0';i++){
+		if(msg[i]>= 'a' && msg[i] <= 'z')
+			encode[i] = encryptLetter(msg[i],key, 'z');
+		else if(msg[i]>= 'A' && msg[i] <= 'Z')
+			encode[i] = encryptLetter(msg[i],key, 'Z');
+		else
+			encode[i] = msg[i];
+	}
+	printf("Encrypted message : %s",encode);
+	for(i=0;encode[i] != '\0';i++){
+		if(encode[i]>= 'a' && encode[i] <= 'z')
+			decode[i] = decryptLetter(encode[i],key, 'a');
+		else if(encode[i]>= 'A' && encode[i] <= 'Z')
+			decode[i] = decryptLetter(encode[i],key, 'A');
+		else
+			decode[i] = encode[i];
+	}
+	printf("\nDecrypted message : %s\n", decode);
+	return 0;
 }
